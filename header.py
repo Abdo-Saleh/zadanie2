6f2def48a28c0125a7fa6c3c18f5ee2f5370c3a4
Abdo-Saleh <micro.saleh@outlook.com>
2020-12-04 11:26:52 +0100

    Header

--- header.py
+++ header.py
@@ -1,8 +1,19 @@
-class Header:
-    def __init__(self, typ):
-        self.typ = typ
+import sys
+from ctypes import sizeof
 
-    def calc(self):
-        print(self.typ)
+
+class Header:
+    def __init__(self, typ, index, number_of_fragments, fragment_size, crc):
+        # 2 bytes
+        self.typ = typ
+        # 4 bytes
+        self.index = index
+        # 4 bytes
+        self.number_of_fragments = number_of_fragments
+        # 4 bytes
+        self.fragment_size = fragment_size
+        # 2 bytes
+        self.crc = crc
+
 
 
